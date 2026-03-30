b0885cd88d27c3522727afa2492c0a27eafaef1c
Thomas Bottini <thomas.bottini@cnrs.fr>
2026-03-30 15:59:46 +0200

    🌴

--- common-files.ts
+++ common-files.ts
@@ -2,7 +2,9 @@ export async function indexFilesByName(folder: string) {
     const o = {};
 
     for await (const entry of Deno.readDir(folder)) {
-        console.log(entry.name, entry.isFile ? "file" : "dir");
+        if (entry.isFile) {
+            o[entry.name] = `${folder}/${entry.name}`
+        }
     }
 
     return o;
